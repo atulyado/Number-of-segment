c4fc2c8bbd1a2f3441ba81245e47dcb56604903b
atulyado <133553524+atulyado@users.noreply.github.com>
2025-06-19 11:33:20 +0700

    Update numberOfSegment.java

--- numberOfSegment.java
+++ numberOfSegment.java
@@ -1,13 +1,13 @@
 class Solution {
     public int countSegments(String s) {
     String input = s;
-    String[] parts = input.split("[,\\s]+");
+    String[] parts = input.split("[,\\s]+");//here it split it from comma and space
     int i =0;
     for(String part : parts){
-     if(!part.isEmpty()){
-        i++;
+     if(!part.isEmpty()){//here is going to count how many parts are here 
+        i++;//each time is going add 1
      }
      }
-    return i;
+    return i;//and return the value here
 }
 }
